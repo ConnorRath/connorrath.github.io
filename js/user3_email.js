099d2effec6de2ba207100e5ce890bbf8e3da6d6
ConnorRath <ccr028@shsu.edu>
2016-10-01 18:32:37 -0500

    Draft Final

--- js/user3_email.js
+++ js/user3_email.js
@@ -2,5 +2,5 @@ function writeToP(nameOfP, stringToPrint) {
     "use strict";
     document.getElementById(nameOfP).innerHTML = (stringToPrint);
 }
-writeToP("narrative", "You are a waste of bandwidth...");
+writeToP("narrative", "Surprising...");
 writeToP("question", "Is this related to Email or Outlook in anyway?");
