b24dbb897af46d7bf3e3fc897f69c71c8645a21a
ConnorRath <ccr028@shsu.edu>
2016-10-01 18:36:00 -0500

    Final Skeleton

--- js/pro1.js
+++ js/pro1.js
@@ -2,5 +2,5 @@ function writeToP(nameOfP, stringToPrint) {
     "use strict";
     document.getElementById(nameOfP).innerHTML = (stringToPrint);
 }
-writeToP("narrative", "Hello Professional, thank you for chosing me for your IT issues");
+writeToP("narrative", "Alright great!");
 writeToP("question", "Would you like to use my age-to-issue automatic sensing technology?");
