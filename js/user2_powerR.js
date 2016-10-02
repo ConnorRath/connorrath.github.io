e31a33b53d8fb66c72fc55f7913d8e462512ac91
ConnorRath <ccr028@shsu.edu>
2016-10-01 20:14:31 -0500

    Beginning BackGround And Font Customization
    Buttons are still trash though...

--- js/user2_powerR.js
+++ js/user2_powerR.js
@@ -2,5 +2,14 @@ function writeToP(nameOfP, stringToPrint) {
     "use strict";
     document.getElementById(nameOfP).innerHTML = (stringToPrint);
 }
+function sleep(milliseconds) {
+    "use strict";
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 writeToP("narrative", "Don't lie to me");
 writeToP("question", "Have you tried restarting it?");
