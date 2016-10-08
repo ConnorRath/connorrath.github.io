1fef4a9cc19aa18da9df4db96d88cb733bc1faf7
ConnorRath <ccr028@shsu.edu>
2016-10-08 09:57:35 -0500

    Remove parralax from all slides but index, and fix footer to bottom

--- js/1-1.js
+++ js/1-1.js
@@ -7,7 +7,7 @@ function writeToP(nameOfP, stringToPrint) {
     "use strict";
     document.getElementById(nameOfP).innerHTML = (stringToPrint);
 }
-writeToP("narrative", "Results inconclusive... But anyways");
+writeToP("narrative", "Results... inconclusive");
 writeToP("question", "Have you tried restarting it?");
 document.getElementById("yes").addEventListener("mousedown", function(){
   setTimeout("window.location.href='1-2.html';", 500);
